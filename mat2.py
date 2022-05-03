7621137932b5609c39088ebe5052a96185b6de3a
vigneshwar-24 <77089276+vigneshwar-24@users.noreply.github.com>
2022-05-03 16:55:23 +0530

    Add files via upload

---
+++ mat2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import math
+import scipy.stats
+X=[0,1,2,3,4,5,6]
+f=[153,169,72,31,12,6,2]
+n=6
+N=np.sum(f)
+mean=np.inner(X,f)/N
+Pr=list(); E=list(); xi=list()
+print("  X P(X=x) Obs.Fr  Ex.Fre   xi ")
+print("----------------------------------")
+for x in range(7):
+    Pr.append(math.exp(-mean)*mean*x/math.factorial(x))
+    E.append(Pr[x]*N)
+    xi.append((f[x]-E[x])**2/E[x])
+    print("%2.2f %2.2f  %4.2f   %3.2f  %3.2f"%(x,Pr[x],f[x],E[x],xi[x]))
+print("----------------------------------")
+cal_chi2=np.sum(xi)
+print("Calculated value of Chi square is %4.2f"%cal_chi2)
+tab_chi2=scipy.stats.chi2.ppf(1-.01, df=n)
+print("Table value of Chi square at 1  level is %4.2f"%tab_chi2)
+if cal_chi2<tab_chi2:
+    print("The given data can be fitted in Poissson distribution at 1% LOS")
+else:
+    print("The given data cannot be fitted in Poisson distribution at 1% LOS")
+
+
+# In[ ]:
+
+
+
+
