2efd1602db69fe02adcb7921b5a73e5e5504ff24
vigneshwar-24 <77089276+vigneshwar-24@users.noreply.github.com>
2022-05-18 11:12:46 +0530

    Update mat2.py

--- mat2.py
+++ mat2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import numpy as np
 import math
 import scipy.stats
@@ -12,14 +6,14 @@ f=[153,169,72,31,12,6,2]
 n=6
 N=np.sum(f)
 mean=np.inner(X,f)/N
-Pr=list(); E=list(); xi=list()
+Prob=list(); E=list(); xi=list()
 print("  X P(X=x) Obs.Fr  Ex.Fre   xi ")
 print("----------------------------------")
 for x in range(7):
-    Pr.append(math.exp(-mean)*mean*x/math.factorial(x))
-    E.append(Pr[x]*N)
+    Prob.append(math.exp(-mean)*mean**x/math.factorial(x))
+    E.append(Prob[x]*N)
     xi.append((f[x]-E[x])**2/E[x])
-    print("%2.2f %2.2f  %4.2f   %3.2f  %3.2f"%(x,Pr[x],f[x],E[x],xi[x]))
+    print("%2.2f %2.2f  %4.2f   %3.2f   %3.2f"%(x,Prob[x],f[x],E[x],xi[x]))
 print("----------------------------------")
 cal_chi2=np.sum(xi)
 print("Calculated value of Chi square is %4.2f"%cal_chi2)
@@ -29,10 +23,3 @@ if cal_chi2<tab_chi2:
     print("The given data can be fitted in Poissson distribution at 1% LOS")
 else:
     print("The given data cannot be fitted in Poisson distribution at 1% LOS")
-
-
-# In[ ]:
-
-
-
-
